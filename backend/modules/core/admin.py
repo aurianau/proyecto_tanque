b98aac9b2b40fa3e6253bea138397ae7566641ad
Roberto <robertoguillot23@gmail.com>
2026-05-08 12:19:33 -0500

    Admin: Agregar filtros de roles en la lista de usuarios

--- backend/modules/core/admin.py
+++ backend/modules/core/admin.py
@@ -5,6 +5,7 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'phone', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
     fieldsets = UserAdmin.fieldsets + (
         ('Información Adicional', {'fields': ('phone', 'role', 'avatar_url')}),
     )
